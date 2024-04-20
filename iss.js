59fb31e7ef3879fe7d61f608a9d3eda936de6929
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-19 20:25:18 -0600

    initial API call implemented

---
+++ iss.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const issURL = 'https://api.ipify.org?format=json';
+
+request(issURL, (error, response, body) => {
+  if (error) return callback(error, null);
+  if (response.statusCode !== 200) {
+    callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+    return;
+  }
+  const ip = JSON.parse(body).ip;
+  callback(null, ip);
+});
+
+
+module.exports = { fetchMyIP };
