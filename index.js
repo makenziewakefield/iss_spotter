a290e10053eef0f6be62b04a431469bc85dc40ba
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-22 15:26:48 -0600

    made a function that prints out the pass times and called the function that determines the passes times

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP,
+        fetchCoordsByIP,
+        fetchISSFlyOverTimes,
+        nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log("It worked! Returned IP: ", ip);
+// });
+
+
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates: ', coordinates);
+// });
+
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log(`It didn't work!`, error);
+//     return;
+//   }
+//   console.log(`It worked! Returned fly over times: `, passTimes);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  console.log("It worked! Returned IP: ", ip);
-})
+};
 
-
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
-  console.log('It worked! Returned coordinates: ', coordinates);
-})
-
-const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
-
-fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-  if (error) {
-    console.log(`It didn't work!`, error);
-    return;
-  }
-  console.log(`It worked! Returned fly over times: `, passTimes);
-})
+  printPassTimes(passTimes);
+});
