e50df4128041c41ce003ca4e992a7b2673ccfb03
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-04-22 14:58:20 -0600

    created function that returns the fly over times of the ISS given certain coordinates.

--- iss.js
+++ iss.js
@@ -29,4 +29,20 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching fly over times. Response: ${body}`), null);
+      return;
+    }
+    const passes = JSON.parse(body). response;
+    callback(null, passes);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
